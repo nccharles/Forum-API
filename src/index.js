09ade7e481920886155a3dd55cb2e03201c6b995
Charles NDAYISABA <nccharles1@gmail.com>
2022-02-06 11:16:32 +0200

    changed date format

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use('/api/v3', frouter);
 const emitMostRecentMessages = async (socket, room) => {
   const result = (await db.getChats('*')).filter(c => c.room === room.id)
   if (result.length) {
-    result.map(i => i.created_at = moment(i.created_at).format('h:mm A'))
+    result.map(i => i.created_at = moment(i.created_at).format('dd MM,YY h:mm A'))
     io.to(room.name).emit('message', result);
   } else {
     socket.emit('message', formatMessage("bot", 'Welcome here is the begginning of your chats'))
